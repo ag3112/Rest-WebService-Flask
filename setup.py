5aa681d80b93336f86c099f8146eaf2a37058f90
Ankit Gupta <ag_3112@outlook.com>
2017-12-23 10:10:43 +0530

    Adding initial directory structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Rest-WebService-Flask',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Ankit',
+    author_email='ag_3112@outlook.com',
+    description='Test Web Service Project'
+)
